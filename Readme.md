47b3c0dcd6dea13b9d5c5e1b56699f92122a8312
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2020-09-02 07:48:08 +0530

    +fixES6 +parserperline

--- Readme.md
+++ Readme.md
@@ -29,6 +29,8 @@ var jsonoutput=parseGDBMIOutput(validgdbmioutputString);
 [{"bkpt":{"number":"1","type":"breakpoint","disp":"keep","enabled":"y","addr":"0x000100d0","func":"main","file":"hello.c","fullname":"/home/foo/hello.c","line":"5","times":"0"},"token":"34","async-type":"result-record","class":"data"},{"bkpt":{"number":"1","type":"breakpoint","disp":"keep","enabled":"y","addr":"0x000100d0","func":"main","file":"hello.c","fullname":"/home/foo/hello.c","line":"5","times":"0"},"token":"34","async-type":"result-record","class":"data"},{"bkpt":{"number":"1","type":"breakpoint","disp":"keep","enabled":"y","addr":"0x000100d0","func":"main","file":"hello.c","fullname":"/home/foo/hello.c","line":"5","times":"0"},"token":"34","async-type":"result-record","class":"data"}]
 ```
 
+### :warning: Caution:
+     This library depends to an ES6 feature,if that feature is found to be missing,it will try to patch by itself.(string.prototype.replaceall)
 ### References:
 - [Output Syntax](https://www.zeuthen.desy.de/dv/documentation/unixguide/infohtml/gdb/GDB_002fMI-Output-Syntax.html#GDB_002fMI-Output-Syntax)
 - [Example Output](https://www.zeuthen.desy.de/dv/documentation/unixguide/infohtml/gdb/GDB_002fMI-Breakpoint-Commands.html#GDB_002fMI-Breakpoint-Commands)
--- index.js
+++ index.js
@@ -4,7 +4,17 @@
 //~console-stream-output :is output that should be displayed as is in the console.
 //@target-stream-output is the output produced by the target program.
 //&log-stream-output: contains on-going status information about the progress of a slow operation. It can be discarded.
-const RE_GDBMI_OUTPUT=()=>/(\d*)?(.)\b([a-zA-Z]*?),([\s\S]*?)\(gdb\)\n/g
+
+//fix
+if (!String.prototype.replaceAll)
+{
+    var es6fix=require('string.prototype.replaceall')
+    String.prototype.replaceAll=function(...arg)
+    {
+        return es6fix(this,...arg)
+    }
+}
+const RE_GDBMI_OUTPUT=()=>/^(\d*)?(.)\b([a-zA-Z/-]*?),([\s\S]*?)(\(gdb\))??\n$/g
 const RE_GDBMI_KEYS=()=>/([^"]|^)\b([\w\-]*)\b(=)/g
 const outputtypeSymbolMap={
     '^':"result-record",
@@ -17,19 +27,19 @@ const outputtypeSymbolMap={
 }
 function parserecord(listText)
 {
-    return JSON.parse(`{${listText.replaceAll(RE_GDBMI_KEYS(),(_,pre,word,eq)=> pre.trim()+'"'+word+'":')}}`)
+    var str=(`{${listText.replaceAll(RE_GDBMI_KEYS(),(_,pre,word,eq)=> pre.trim()+'"'+word+'":')}}`)
+    return  JSON.parse(str)
 }
-function parseGDBMIOutput(text)
+
+function parseGDBMIOutputLine(line)
 {
     try{
-        var results=text.matchAll(RE_GDBMI_OUTPUT())
-        var outputRecordDict=[...results].map(([_,token,asyncOutputSymbol,resultClass,recordList])=>
-        Object.assign(parserecord(recordList),{token,'async-type':outputtypeSymbolMap[asyncOutputSymbol],"class":resultClass}))
-         return outputRecordDict
+        var [_,token,asyncOutputSymbol,resultClass,recordList,isLast]=RE_GDBMI_OUTPUT().exec(line)
+        return Object.assign(parserecord(recordList),{token,'async-type':outputtypeSymbolMap[asyncOutputSymbol],"class":resultClass,sequenceEnd:!!isLast})
     }
     catch(e)
     {
         throw "Illegal GDGMI output string!\nString must match 'RE_GDBMI_OUTPUT'"
     }
 }
-module.exports={parseGDBMIOutput,parserecord}
+module.exports={parseGDBMIOutputLine,parseLine:parseGDBMIOutputLine,parserecord}
